6a7b849015c31bf32691c20c5225e4c86a617bc9
Jack Cooper-McEwan <jackcooper93@gmail.com>
2023-01-09 16:32:29 +0000

    it works!

--- linechart.js
+++ linechart.js
@@ -1,109 +1,65 @@
-
 // set the dimensions and margins of the graph
-var width = 1000
-    height = 1000
-    margin = 40
+const width = 1000;
+const height = 1000;
+const margin = 40;
 
-// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-//const radius = Math.min(width, height) / 2 - margin
-const radius = 450
-const innerRadius = 300
+const radius = 450;
+const innerRadius = 300;
 
-// append the svg object to the div called 'my_dataviz'
-const svg = d3.select(".chart")
-//  .append("svg")
-//    .attr("width", width)
-//    .attr("height", height)
-//.attr("preserveAspectRatio", "xMinYMin meet")
-//.attr("viewBox", "0 0 1000 1000")
+const svg = d3.select("svg")
   .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 const dayOfYear =
-  Math.floor((new Date() - new Date((new Date()).getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
-const innerData = {'completed': dayOfYear-1, 'togo': 365 - dayOfYear + 1}
+  Math.floor(
+    (new Date() - new Date((new Date()).getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
+const innerData = {
+  "completed": dayOfYear - 1,
+  "togo": 365 - dayOfYear + 1
+};
 
-function color(key){
-  if (key == 'completed'){
-  return "#4CAF50"}
-  return "#CFD8DC"
+function draw(data, inner, outer, selector){
+  const pie = d3.pie().sort(null)
+    .value(d => d.value);
+  return svg
+    .selectAll(selector)
+    .data(pie(d3.entries(data)))
+    .enter()
+    .append("path")
+    .attr("d", d3.arc()
+      .innerRadius(inner) // This is the size of the donut hole
+      .outerRadius(outer)
+    )
+    .attr("class", (d) => d.data.key);
 }
 
-function opacity(key){
-  if (key=='completed'){
-  return 0.7}
-  return 0.3
-}
-
-const pie = d3.pie().sort(null)
-  .value(function(d) {return d.value; })
-
-function drawPushUps(pushupData){
-
-var data_ready = pie(d3.entries(pushupData))
-svg
-  .selectAll('whatever')
-  .data(data_ready)
-  .enter()
-  .append('path')
-  .attr('d', d3.arc()
-    .innerRadius(innerRadius)         // This is the size of the donut hole
-    .outerRadius(radius)
-  )
-  .attr('fill', function(d){ return(color(d.data.key)) })
-  .attr("stroke", "black")
-  .style("stroke-width", "2px")
-  .style("stroke-opacity", 0.3)
-  .style("opacity", d => opacity(d.data.key));
-
-
-}
-
-function drawText(number){
+function drawText(number) {
   const text = svg.append("text")
-   .attr("text-anchor", "middle")
-   .text(0);
-
-   text
-   .transition()
-  .tween("text", function() {
-     var selection = d3.select(this);    // selection of node being transitioned
-     var start = d3.select(this).text(); // start value prior to transition
-     var end = number;                     // specified end value
-     var interpolator = d3.interpolateNumber(start,end); // d3 interpolator
-     return function(t) {selection.text(Math.round(interpolator(t)).toLocaleString());};  // return value
-
-  })
-  .duration(1000);
+    .attr("text-anchor", "middle")
+    .text(0);
+  text
+    .transition()
+    .tween("text", function() {
+      var selection = d3.select(this); // selection of node being transitioned
+      var start = d3.select(this).text(); // start value prior to transition
+      var end = number; // specified end value
+      var interpolator = d3.interpolateNumber(start, end); // d3 interpolator
+      return function(t) {
+        selection.text(Math.round(interpolator(t)).toLocaleString());
+      };
+    })
+    .duration(1000);
 }
 
-
-function drawInner(){
-var data_ready = pie(d3.entries(innerData))
-
-svg
-  .selectAll('inner')
-  .data(data_ready)
-  .enter()
-  .append('path')
-  .attr('d', d3.arc()
-    .innerRadius(innerRadius-5)         // This is the size of the donut hole
-    .outerRadius(innerRadius)
-  )
-  .attr('fill', function(d){ return(color(d.data.key)) })
-  .style("opacity", d => opacity(d.data.key))
+function parseData(data) {
+  var d = d3.sum(data, (d) => d.pushups);
+  var pushupData = {
+    "completed": d,
+    "togo": 10000 - d
+  };
+  draw(pushupData, innerRadius, radius, "outer").classed("outer", true);
+  draw(innerData, innerRadius-5, innerRadius, "inner");
+  drawText(pushupData.completed);
 }
 
-function parseData(data){
-var d = d3.sum(data, d=> d.pushups)
-var pushupData = {'completed': d, 'togo': 10000-d}
-drawPushUps(pushupData)
-drawInner()
-drawText(pushupData.completed)
-
-
-
-//console.log(pushupData)
-}
-
-d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTcCYisJ3ihPZ5ObmJbvNitDOp8eEB93Gf3CU9odNlv2ia-qALPv0dmAEQLklNnLfpS-TQJyYyDT8Xl/pub?gid=0&single=true&output=csv", 0,parseData)
+d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTcCYisJ3ihPZ5ObmJbvNitDOp8eEB93Gf3CU9odNlv2ia-qALPv0dmAEQLklNnLfpS-TQJyYyDT8Xl/pub?gid=0&single=true&output=csv", 0, parseData);
